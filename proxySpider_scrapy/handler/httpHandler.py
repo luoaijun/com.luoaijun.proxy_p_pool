2be403d3d4ad3071ce2a9e3d6b49381e638b7c10
luoaijun <3191287315@qq.com>
2019-10-16 10:33:23 +0800

    update:add a type

--- proxySpider_scrapy/handler/httpHandler.py
+++ proxySpider_scrapy/handler/httpHandler.py
@@ -13,11 +13,13 @@ class getAllHandler(tornado.web.RequestHandler):
 
     def get(self):
         """get请求"""
-        sql = "select ip,port from ip_pool.alive_ip"
+        size = self.get_argument('size')
+        type = self.get_argument('type')
+        sql = "select ip,port from ip_pool.alive_ip where type = " + "\"" + type + "\""
         list = []
         mysql.connect()
         mysql.cursor.execute(sql)
-        size = self.get_argument('size')
+
         data = mysql.cursor.fetchmany(int(size))
         for i in range(0, int(size)):
             print(i)
@@ -26,7 +28,32 @@ class getAllHandler(tornado.web.RequestHandler):
         self.write(json.dumps(list))
 
 
-application = tornado.web.Application([(r"/getBySize", getAllHandler)])
+class getUrl(tornado.web.RequestHandler):
+
+    def get(self):
+        """get请求"""
+        size = self.get_argument('size')
+        type = self.get_argument('type')
+        sql = "select url from ip_pool.alive_ip where type = " + "\"" + type + "\""
+        list = []
+        mysql.connect()
+        mysql.cursor.execute(sql)
+        data = mysql.cursor.fetchmany(int(size))
+        for i in range(0, int(size)):
+            print(i)
+            jsonStr = {"url": data[i][0]}
+            list.append(jsonStr)
+        self.write(json.dumps(list))
+
+
+class test(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world，test success！")
+
+
+application = tornado.web.Application([(r"/getBySize", getAllHandler),
+                                       (r"/test", test),
+                                       (r"/getUrl", getUrl)])
 
 
 def run():
