28254a9bde83500fdb5de1ab094041550340ca68
luoaijun <3191287315@qq.com>
2019-10-18 17:03:20 +0800

    update

--- proxySpider_scrapy/handler/httpHandler.py
+++ proxySpider_scrapy/handler/httpHandler.py
@@ -15,14 +15,13 @@ class getAllHandler(tornado.web.RequestHandler):
         """get请求"""
         size = self.get_argument('size')
         type = self.get_argument('type')
-        sql = "select ip,port from ip_pool.alive_ip where type = " + "\"" + type + "\""
+        sql = "select ip,port from ip_pool.alive_ip where type = " + "\"" + type + "\" " + " limit " + size
         list = []
         mysql.connect()
         mysql.cursor.execute(sql)
 
-        data = mysql.cursor.fetchmany(int(size))
-        for i in range(0, int(size)):
-            print(i)
+        data = mysql.cursor.fetchall()
+        for i in range(0, len(data)):
             jsonStr = {"ip": data[i][0], "port": data[i][1]}
             list.append(jsonStr)
         self.write(json.dumps(list))
@@ -49,6 +48,7 @@ class getUrl(tornado.web.RequestHandler):
 class test(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello world，test success！")
+        print("success")
 
 
 application = tornado.web.Application([(r"/getBySize", getAllHandler),
